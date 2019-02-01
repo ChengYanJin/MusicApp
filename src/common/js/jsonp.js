db763f389a2ae61e1afbf8e6ab6a81e66c215b5b
Chengyanjin_i <chengyanjin_i@didiglobal.com>
2019-02-01 18:40:05 +0800

    add recommendation

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJSON from 'jsonp'
+
+export default function jsonp(url, data, option) {
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+    return Promises((resolve, reject) => {
+        originJSON(url, option, (err, data) => {
+            if (!err) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
+
+    })
+}
+
+function param(data) { // json => url
+    let url = ''
+    for (var k in data) {
+        let value = data[k] !== undefined ? data[k] : ''
+        url += `&${k}=${encodeURIComponent(value)}`
+    }
+    return url ? url.substring(1) : ''
+}
